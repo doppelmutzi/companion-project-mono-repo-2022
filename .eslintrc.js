ddfb3c9f7c3f594e82a9d1718b50bbf723c95964
Sebastian Weber <sebatian.weber80@gmail.com>
2022-03-11 20:26:09 +0100

    next version of Yarn Berry mono-repo demo

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,20 +1,3 @@
 module.exports = {
-  env: {
-    browser: true,
-    es2021: true,
-  },
-  extends: [
-    "plugin:react/recommended",
-    "standard",
-    "plugin:prettier/recommended",
-  ],
-  parser: "@babel/eslint-parser",
-  parserOptions: {
-    ecmaFeatures: {
-      jsx: true,
-    },
-    ecmaVersion: 12,
-    sourceType: "module",
-  },
-  plugins: ["react"],
-};
+    extends: ['@doppelmutzi/eslint-config']
+}
